3118c6f12d11a7a8dd46e67ef951841f9869d976
astra34 <Yassine.qejiou@outlook.fr>
2024-02-13 23:27:18 +0100

    debug

--- App.js
+++ App.js
@@ -38,7 +38,8 @@ app.use(session({
     maxAge: 86400000,
     httpOnly: true,
     secure: true,
-    domain: 'https://astra34.github.io/MyApp/'
+    domain: process.env.ALLOW_URL,
+    sameSite: 'none'
   }
 }));
 
